9e8920c14052fecc5b1c723a9fde0b3bc9bb5e4c
Hugo Dorfling <hugodorfling@live.co.za>
2022-07-01 06:30:39 +0200

    Add Style loaders and config rules to webpack for images, svgs, fonts.

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,18 @@
+import "./styles.css";
+import MyImage from "./assets/me.jpg";
+import LOGO from "./assets/logo.svg";
+
 export const App = () => {
-  return <h1>Hugo Dorfling Portfolio</h1>;
+  return (
+    <div>
+      <h1>Hugo Dorfling Portfolio</h1>
+      <img
+        src={MyImage}
+        alt="Image of Hugo"
+        width="300"
+        height="300"
+      />
+      <img src={LOGO} alt="Logo Image" width="300" height="300" />
+    </div>
+  );
 };
